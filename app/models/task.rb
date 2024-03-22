88ac301aa67788db7328bcad8d3ac5bd85a0f801
U-dias <nomd_adniss@yahoo.co.jp>
2024-03-23 05:53:10 +0900

    バリデーション訂正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :title, :description, :start, :end, presence:  { message: "を入力して下さい。" }
   validates :title, length: { maximum: 20, message: "は20字までにして下さい。"}
-  validates :description, length: { maximum: 5, message: "は500字までにして下さい。"}
+  validates :description, length: { maximum: 500, message: "は500字までにして下さい。"}
   # validates :end, numericality: { greater_than_or_equal_to: :start,message: "が間違っています。" }
   validate :start_end_check
 
